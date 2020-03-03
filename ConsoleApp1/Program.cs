ec155ecf641d5cb75cfd8107d1c6f1e7fe104903
alexandrzavala@gmail.com <alexandrzavala@gmail.com>
2020-03-03 12:27:29 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.WriteLine("Enter a - ");
+            a= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter b - ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Equal - " + (a*b));
+        }
+    }
+}
